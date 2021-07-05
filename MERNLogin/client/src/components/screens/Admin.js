e96ae2f1653442998716dfaa8e77a7e836aca338
HarshanaWalpita <harshanawalpita@gmail.com>
2021-07-05 17:34:03 +0530

    mern login

---
+++ MERNLogin/client/src/components/screens/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrivateScreen from "./PrivateScreen";
+
+function Admin() {
+    return (
+        <>
+            <h3>Admin</h3>
+        </>
+    );
+}
+
+export default Admin;
---
+++ MERNLogin/client/src/components/screens/Buyer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Buyer extends Component{
+    render(){
+        return(
+            <>
+            <h3>Buyer</h3>
+            </>
+        )
+    }
+}
+
